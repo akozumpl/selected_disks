f716d3ec5ab1c383a37853d5b84f0110844ef8e9
Ales Kozumplik <akozumpl@redhat.com>
2011-10-03 15:44:23 +0200

    initial commit

---
+++ w.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python
+
+import glib
+from gi.repository import Gtk, Gdk
+
+main_loop = None
+
+def f_quit(widget):
+    main_loop.quit()
+
+def c_button(button):
+    builder = Gtk.Builder()
+    builder.add_from_file("selected_disks.glade")
+    w = builder.get_object("dialog1")
+    store = builder.get_object("liststore_disks")
+    iter = store.insert(0)
+    store.set(iter, {0 : "yeah"})
+    w.show_all()
+
+def main():
+    button = Gtk.Button("show")
+    button.connect('clicked', c_button)
+    window = Gtk.Window(title="W")
+    window.connect('destroy', f_quit)
+    window.add(button)
+    window.show_all()
+    global main_loop
+    main_loop= glib.MainLoop()
+    main_loop.run()
+
+if __name__ == "__main__":
+    main()
