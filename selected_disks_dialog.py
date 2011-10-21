23de353fad68d3673aa00910efd313e2478a0951
Ales Kozumplik <akozumpl@redhat.com>
2011-10-21 10:10:21 +0200

    Fix method naming conventions.
    Public methods, methods that are overriden in derived classes and Glade
    callbacks start with a letter. The rest start with an underscore.

--- selected_disks_dialog.py
+++ selected_disks_dialog.py
@@ -19,8 +19,8 @@ class SelectedDisksDialog(object):
         self.view = builder.get_object("treeview_disks")
         self.store = builder.get_object("treestore_disks")
         self.label = builder.get_object("label_summary")
-        self.store.set_sort_func(self.COL_CAPACITY, self.cmp_device, "size")
-        self.store.set_sort_func(self.COL_FREE, self.cmp_device, "size")
+        self.store.set_sort_func(self.COL_CAPACITY, self._cmp_device, "size")
+        self.store.set_sort_func(self.COL_FREE, self._cmp_device, "size")
 
     def _append_device(self, device, it_parent):
         it = self.store.append(it_parent)
@@ -30,30 +30,7 @@ class SelectedDisksDialog(object):
         self.store.set_value(it, self.COL_FREE, "%d GB" % (device.size / 1000))
         self.store.set_value(it, self.COL_ID, device.serial)
 
-    def _iter_device_rows(self):
-        """ Iterator for those rows of model that represent a real device. """
-        return (it for it in self.store)
-
-    def _update_label(self):
-        vals = {
-            "count" : len(list(self._iter_device_rows())),
-            "capacity" : reduce(lambda acc, row: acc + row[self.COL_OBJECT].size,
-                                self._iter_device_rows(), 0) / 1000,
-            "free" : reduce(lambda acc, row: acc + row[self.COL_OBJECT].size,
-                                self._iter_device_rows(), 0) / 1000
-            }
-        self.label.set_markup(self.SUMMARY_TEMPLATE % vals)
-
-    def cb_close(self, button):
-        self.window.destroy()
-
-    def cb_remove(self, button):
-        path = self.view.get_cursor()[0]
-        it = self.store.get_iter(path)
-        self.store.remove(it)
-        self.update()
-
-    def cmp_device(self, model, a_iter, b_iter, attr):
+    def _cmp_device(self, model, a_iter, b_iter, attr):
         def compute_for_iter(it):
             device = self.store.get_value(it, self.COL_OBJECT)
             if device:
@@ -69,6 +46,29 @@ class SelectedDisksDialog(object):
 
         return compute_for_iter(a_iter) - compute_for_iter(b_iter)
 
+    def _update_label(self):
+        vals = {
+            "count" : len(list(self.iter_device_rows())),
+            "capacity" : reduce(lambda acc, row: acc + row[self.COL_OBJECT].size,
+                                self.iter_device_rows(), 0) / 1000,
+            "free" : reduce(lambda acc, row: acc + row[self.COL_OBJECT].size,
+                                self.iter_device_rows(), 0) / 1000
+            }
+        self.label.set_markup(self.SUMMARY_TEMPLATE % vals)
+
+    def cb_close(self, button):
+        self.window.destroy()
+
+    def cb_remove(self, button):
+        path = self.view.get_cursor()[0]
+        it = self.store.get_iter(path)
+        self.store.remove(it)
+        self.update()
+
+    def iter_device_rows(self):
+        """ Iterator for those rows of model that represent a real device. """
+        return (it for it in self.store)
+
     def populate(self, devices):
         map(lambda d : self._append_device(d, None), devices)
         self.update()
@@ -78,13 +78,13 @@ class SelectedDisksDialog(object):
         self.window.show_all()
         self.window.run()
         self.window.destroy()
-        return [r[self.COL_OBJECT] for r in self._iter_device_rows()]
+        return [r[self.COL_OBJECT] for r in self.iter_device_rows()]
 
     def update(self):
         self._update_label()
 
 class SelectedDisksTreeDialog(SelectedDisksDialog):
-    def _iter_device_rows(self):
+    def iter_device_rows(self):
         """ Iterator for those rows of model that represent a real device.
 
             Categories, namely, are excluded.
